a68713d26edae301bf11576f3b209854af7ff8b5
mengfuzijiadao <mengfanzhi1232022@163.com>
2024-08-26 22:41:30 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   api/get_env.py

--- api/get_env.py
+++ api/get_env.py
@@ -3,10 +3,10 @@ import os
  
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-	try:
-		server_name = os.environ.get('server_name')
-	except Exception as E:
-		server_name = repr(E)
+        try:
+            server_name = os.environ.get('server_name')
+        except Exception as E:
+            server_name = repr(E)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
