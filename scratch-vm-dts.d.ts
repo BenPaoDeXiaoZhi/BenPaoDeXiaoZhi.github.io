c8fbeac1c81591f289e677d13ef6c942ca2129e1
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-07-25 13:03:15 +0800

    更新 scratch-vm-dts.d.ts

--- scratch-vm-dts.d.ts
+++ scratch-vm-dts.d.ts
@@ -7,8 +7,9 @@
 //   ../node:events
 //   ../jszizp
 //monaco似乎不能处理import…from
-declare type Runtime = import("scratch-vm-dts/engine/runtime")
-declare var Runtime:typeof import("scratch-vm-dts/engine/runtime");
+declare type Runtime = import("scratch-vm-dts/engine/runtime");
+declare var Runtime: typeof import("scratch-vm-dts/engine/runtime");
+
 declare module 'scratch-vm-dts' {
     export = VirtualMachine;
     import VirtualMachine = require("scratch-vm-dts/virtual-machine");
