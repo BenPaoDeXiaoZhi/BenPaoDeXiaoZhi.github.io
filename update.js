e7ea2d6d35590d030efd6ef02aba8077b88d8309
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-12 00:31:49 +0800

    更新 update.js

--- update.js
+++ update.js
@@ -11,6 +11,23 @@ const update = ({ vm, registerSettings}) => {
                 description: "输入id或url，更新扩展",
                 items: [],
             },
+            {
+                key: "pluginFileUrl",
+                label: "url or id",
+                type: "input",
+              description: "url o id",
+              inputProps: {
+                placeholder: "https://m.ccw.site/plugins/...",
+                onPressEnter: (e) => {
+                  (e.target as HTMLInputElement).blur();
+                },
+                onBlur: (e) => {
+                  load(e.target.value,true);
+                },
+              },
+              value: "",
+              autoSave: false,
+            },
         ],
         "",)
         return {
