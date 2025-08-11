509b892fc203091b17e8c69f959c6e6c8389b988
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-12 00:59:59 +0800

    更新 update.js

--- update.js
+++ update.js
@@ -2,6 +2,7 @@ const update = ({ vm, registerSettings}) => {
         console.log(vm.runtime.extensionManager)
         const mgr=vm.runtime.extensionManager
         const load = mgr.loadExtensionURL
+        console.log(load)
         const register = registerSettings("更新特定扩展",
         "plugin-upadte-extension",
         [
@@ -11,23 +12,23 @@ const update = ({ vm, registerSettings}) => {
                 description: "输入id或url，更新扩展",
                 items: [
                         {
-                key: "pluginFileUrl",
-                label: "url or id",
-                type: "input",
-              description: "url o id",
-              inputProps: {
-                placeholder: "https://m.ccw.site/plugins/...",
-                onPressEnter: (e) => {
-                  e.target.blur();
-                },
-                onBlur: (e) => {
-                  load(e.target.value,true);
-                },
-              },
-              value: "",
-              autoSave: false,
-            }
-                ],
+                        key: "pluginFileUrl",
+                        label: "url or id",
+                        type: "input",
+                        description: "url o id",
+                        inputProps: {
+                                placeholder: "https://m.ccw.site/plugins/...",
+                                onPressEnter: (e) => {
+                                  e.target.blur();
+                                },
+                                onBlur: (e) => {
+                                  load(e.target.value,true);
+                                },
+                        },
+                      value: "",
+                      autoSave: false,
+                    }
+                ]
             }
         ],
         "",)
