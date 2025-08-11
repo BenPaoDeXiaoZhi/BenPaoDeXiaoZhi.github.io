3eb5d5e4fad24187e4986cfd3216366125fb464e
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-12 00:28:03 +0800

    更新 update.js

--- update.js
+++ update.js
@@ -1,20 +1,22 @@
 const update = ({ vm, registerSettings}) => {
-	console.log(vm.runtime.extensionManager)
-	const register = registerSettings("更新特定扩展",
+        console.log(vm.runtime.extensionManager)
+        const mgr=vm.runtime.extensionManager
+        const load = mgr.loadExtensionURL
+        const register = registerSettings("更新特定扩展",
         "plugin-upadte-extension",
         [
             {
                 key: "plugin-upadte-extension",
                 label: "更新特定扩展",
-                description: "点击右工作区右边最下面的图标，输入扩展id以更新",
+                description: "输入id或url，更新扩展",
                 items: [],
             },
         ],
         "",)
-	return {
-		dispose(){
-			register.dispose()
-		}
-	}
+        return {
+                dispose(){
+                        register.dispose()
+                }
+        }
 }
 Scratch.plugins.register(update)
