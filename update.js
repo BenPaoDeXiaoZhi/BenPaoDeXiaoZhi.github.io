17d44d9e800dfb1c83b6f83850b4866bebb19a1c
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-08-13 17:46:47 +0800

    Update update.js
    实现了更新扩展至最新版本

--- update.js
+++ update.js
@@ -1,9 +1,7 @@
-const update = ({ vm, registerSettings}) => {
-        console.log(vm.runtime.extensionManager)
-        const mgr=vm.runtime.extensionManager
-        const load = mgr.loadExternalExtensionById
-        console.log(load)
-        const register = registerSettings("更新特定扩展",
+const update = ({ vm, registerSettings }) => {
+    console.log(vm.runtime.extensionManager)
+    const mgr = vm.runtime.extensionManager
+    const register = registerSettings("更新特定扩展",
         "plugin-upadte-extension",
         [
             {
@@ -11,33 +9,36 @@ const update = ({ vm, registerSettings}) => {
                 label: "更新特定扩展",
                 description: "输入id或url，更新扩展",
                 items: [
-                        {
+                    {
                         key: "pluginFileUrl",
-                        label: "url or id",
+                        label: "id",
                         type: "input",
-                        description: "url o id",
+                        description: "id",
                         inputProps: {
-                                placeholder: "https://m.ccw.site/plugins/...",
-                                onPressEnter: (e) => {
-                                  e.target.blur();
-                                },
-                                onBlur(e){
-                                  console.log(e)
-                                  load.call(mgr,e.target.value,true);
-alert(`扩展${e.target.value}已更新`)
-                                },
+                            placeholder: "扩展id",
+                            onPressEnter: (e) => {
+                                e.target.blur();
+                            },
+                            onBlur(e) {
+                                (async()=>{
+                                    console.log(e)
+                                    mgr.loadExtensionURL(await vm.runtime.ccwAPI.getExtensionURLById('spineAnimation'),true)
+                                    alert(`扩展${e.target.value}已更新`)
+                                })()
+                                
+                            },
                         },
-                      value: "",
-                      autoSave: false,
+                        value: "",
+                        autoSave: false,
                     }
                 ]
             }
         ],
         "",)
-        return {
-                dispose(){
-                        register.dispose()
-                }
+    return {
+        dispose() {
+            register.dispose()
         }
+    }
 }
 Scratch.plugins.register(update)
