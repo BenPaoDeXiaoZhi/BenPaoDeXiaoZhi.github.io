db3413ceb900d77e25c1c53361c939822d415079
BenPaoDeXiaoZhi <BenPaoDeXiaoZhi@users.noreply.github.com>
2026-06-01 01:37:00 +0000

    fix: use svelte.config.js to fix CI type check error

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
-import type { Config } from '@sveltejs/kit';
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	compilerOptions: {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
