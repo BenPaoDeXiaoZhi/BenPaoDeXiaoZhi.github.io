a358b375d748c1a87ab65bf563220aebbaefb43d
BenPaoDeXiaoZhi <BenPaoDeXiaoZhi@users.noreply.github.com>
2026-06-05 13:21:54 +0000

    feat: initialize SvelteKit project with static adapter and CI

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+	},
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
